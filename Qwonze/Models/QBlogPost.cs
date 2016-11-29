786284e33aadda88c73fd1f4e08e0cba9221fcdd
Reyth3 <mrreynevan2@gmail.com>
2016-11-29 21:55:33 +0100

    Added Basic Models; Database Context Info; NuGet Packages

---
+++ Qwonze/Models/QBlogPost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Qwonze.Models
+{
+    public class QBlogPost
+    {
+        public QBlogPost() { }
+        public QBlogPost(string title, string text, string category)
+        {
+            Title = title;
+            Text = text;
+            Category = category;
+            PostingDate = DateTime.UtcNow;
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime PostingDate { get; set; }
+        public string PostedOn { get { return "Posted On: " + PostingDate.ToShortDateString(); } }
+        public string Category { get; set; }
+    }
+}
---
+++ Qwonze/Models/QwonzeContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Qwonze.Models
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class QwonzeContext : DbContext
+    {
+        public QwonzeContext()
+        {
+            Database.CreateIfNotExists();
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<QwonzeContext>());
+        }
+
+        public virtual DbSet<QBlogPost> Posts { get; set; }
+    }
+}
